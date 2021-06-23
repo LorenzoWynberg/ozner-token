90f75d707608d1d4f2f63bb0e0a79dc7369c2575
LorenzoWynberg <lwcincr93@gmail.com>
2021-06-22 20:49:14 -0600

    Added transfer functionality

--- contracts/OznerToken.sol
+++ contracts/OznerToken.sol
@@ -9,8 +9,29 @@ contract OznerToken {
 
   mapping(address => uint256) public balanceOf;
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
+  }
+
+  // Transfer
+  function transfer(address _to, uint256 _value) public returns(bool success) {
+
+    // Exception if account doesn't have enough funds
+    require(balanceOf[msg.sender] >= _value, "Invalid amount");
+
+    // Transfer the balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    // Transfer events
+    emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
--- test/ozner_token.js
+++ test/ozner_token.js
@@ -6,11 +6,11 @@ const OznerToken = artifacts.require("OznerToken");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("OznerToken", function (accounts) {
-    
+
   /**
-  * Sets values:
-  * name, symbol, standard
-  */
+   * Sets values:
+   * name, symbol, standard
+   */
   it('Initializes contract with proper values', async () => {
     let tokenInstance = await OznerToken.deployed();
     let tokenName = await tokenInstance.name();
@@ -32,4 +32,45 @@ contract("OznerToken", function (accounts) {
     let adminBalance = await tokenInstance.balanceOf(accounts[0]);
     assert.equal(adminBalance.toNumber(), 1000000, 'Alocates initial supply to the admin account');
   });
+
+  /**
+   * Transfers token ownership:
+   * Sends 250,000 tokens from accounts[1]
+   * to accounts[2]
+   */
+  it('Transfers token ownership', async () => {
+    let tokenInstance = await OznerToken.deployed();
+    let totalSupply = await tokenInstance.totalSupply();
+    totalSupply = totalSupply.toNumber();
+
+    try {
+      //Try to send double the total supply
+      await tokenInstance.transfer.call(accounts[1], totalSupply*2);
+    } catch (err) {
+      assert(err.message.indexOf('revert') >= 0, 'Invalid transfer amount');
+    }
+
+    //Transfer 250,000 tokens from accounts[1] to accounts[2]
+    let transfer = await tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] })
+    assert.equal(transfer, true, 'Transfer success returns true');
+
+    //Check receipt data
+    let receipt = await tokenInstance.transfer(accounts[1], 250000, {from:accounts[0]});
+    assert.equal(receipt.logs.length, 1, 'Triggers one event');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the transfer event');
+    assert.equal(receipt.logs[0].args._from, accounts[0], 'Logs the sender');
+    assert.equal(receipt.logs[0].args._to, accounts[1], 'Logs the receiver');
+    assert.equal(receipt.logs[0].args._value, 250000, 'Logs the amount transferred');
+
+    //Checks that amount was credited to receiver
+    let balanceReceiver = await tokenInstance.balanceOf(accounts[1]);
+    balanceReceiver = balanceReceiver.toNumber();
+    assert.equal(balanceReceiver, 250000, 'Adds amount to receiving account');
+
+    //Checks that amount was deducted from sender
+    let balanceSender = await tokenInstance.balanceOf(accounts[0]);
+    balanceSender = balanceSender.toNumber();
+    assert.equal(balanceSender, 750000, 'Deducts amount from sender account');
+  });
+
 });
