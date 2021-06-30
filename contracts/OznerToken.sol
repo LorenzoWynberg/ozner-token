196e00ecbf39cc7287ddf8b96fafc1907df57310
LorenzoWynberg <lwcincr93@gmail.com>
2021-06-30 12:11:28 -0600

    Added delegated transfer functionality

--- contracts/OznerToken.sol
+++ contracts/OznerToken.sol
@@ -8,10 +8,17 @@ contract OznerToken {
   uint256 public totalSupply;
 
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   event Transfer(
     address indexed _from,
     address indexed _to,
+    uint256 _value
+  );
+
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
     uint256 _value
   );
 
@@ -33,5 +40,39 @@ contract OznerToken {
     // Transfer events
     emit Transfer(msg.sender, _to, _value);
     return true;
+  }
+
+  // Approve
+  function approve(address _spender, uint256 _value) public returns(bool success) {
+
+    // Set allowance
+    allowance[msg.sender][_spender] = _value;
+
+    // Approve event
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  //Transfer From
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    
+    // Validate that _value is lower than account balance
+    require(_value <= balanceOf[_from], "Cannot transfer more than available balance");
+
+    // Validate that _value is lower than approved
+    require(_value <= allowance[_from][msg.sender], "Cannot transfer more than approved");
+
+    // Update balances
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    // Update allowance
+    allowance[_from][msg.sender] -= _value;
+    
+    // Transfer event
+    emit Transfer(_from, _to, _value);
+
+    return true; 
   }
 }
--- test/ozner_token.js
+++ test/ozner_token.js
@@ -44,17 +44,17 @@ contract("OznerToken", function (accounts) {
     totalSupply = totalSupply.toNumber();
 
     try {
-      //Try to send double the total supply
+      // Try to send double the total supply
       await tokenInstance.transfer.call(accounts[1], totalSupply*2);
     } catch (err) {
       assert(err.message.indexOf('revert') >= 0, 'Invalid transfer amount');
     }
 
-    //Transfer 250,000 tokens from accounts[1] to accounts[2]
+    // Transfer 250,000 tokens from accounts[1] to accounts[2]
     let transfer = await tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] })
     assert.equal(transfer, true, 'Transfer success returns true');
 
-    //Check receipt data
+    // Checks logs
     let receipt = await tokenInstance.transfer(accounts[1], 250000, {from:accounts[0]});
     assert.equal(receipt.logs.length, 1, 'Triggers one event');
     assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the transfer event');
@@ -62,15 +62,111 @@ contract("OznerToken", function (accounts) {
     assert.equal(receipt.logs[0].args._to, accounts[1], 'Logs the receiver');
     assert.equal(receipt.logs[0].args._value, 250000, 'Logs the amount transferred');
 
-    //Checks that amount was credited to receiver
+    // Checks that amount was credited to receiver
     let balanceReceiver = await tokenInstance.balanceOf(accounts[1]);
     balanceReceiver = balanceReceiver.toNumber();
     assert.equal(balanceReceiver, 250000, 'Adds amount to receiving account');
 
-    //Checks that amount was deducted from sender
+    // Checks that amount was deducted from sender
     let balanceSender = await tokenInstance.balanceOf(accounts[0]);
     balanceSender = balanceSender.toNumber();
     assert.equal(balanceSender, 750000, 'Deducts amount from sender account');
   });
 
+  /*
+   *
+   *
+   */
+  it('Approves tokens for delegated transfer', async () => {
+    let tokenInstance = await OznerToken.deployed();
+    let success = await tokenInstance.approve.call(accounts[1], 100);
+    assert.equal(success, true, 'Returns True');
+
+    let receipt = await tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+    assert.equal(receipt.logs.length, 1, 'Triggers one event');
+    assert.equal(receipt.logs[0].event, 'Approval', 'Should be the approval event');
+    assert.equal(receipt.logs[0].args._owner, accounts[0], 'Logs the account the tokens are authorized by');
+    assert.equal(receipt.logs[0].args._spender, accounts[1], 'Logs the account the tokens are authorized to');
+    assert.equal(receipt.logs[0].args._value, 100, 'Logs the amount transferred');
+
+    let allowance = await tokenInstance.allowance(accounts[0], accounts[1]);
+    allowance = allowance.toNumber();
+    assert.equal(allowance, 100, 'Stores the allowance for delegated transfer');
+  });
+
+  it('Handles delegated token transfers', async () => {
+    let tokenInstance = await OznerToken.deployed();
+
+    let main = accounts[0];
+    let from = accounts[2];
+    let to = accounts[3];
+    let spender = accounts[4];
+
+    // Transfer some tokens to from account
+    tokenInstance.transfer(from, 100, { from: main });
+
+    // Approve spender to spend 10 tokens from from account
+    tokenInstance.approve(spender, 10, { from: from });
+
+    // Transfer more than sender's balance
+    try {
+      await tokenInstance.transferFrom(from, to, 99999, { from: spender });
+    } catch (err) {
+      assert(err.message.indexOf('revert') >= 0, 'Cannot transfer more than available balance');
+    }
+
+    // Transfer more than approved amount
+    try {
+      await tokenInstance.transferFrom(from, to, 20, { from: spender });
+    } catch (err) {
+      // console.log(err);
+      assert(err.message.indexOf('revert') >= 0, 'Cannot transfer more than approved');
+    }
+
+    // Check if proper transaction returns true
+    let success = await tokenInstance.transferFrom.call(from, to, 10, { from: spender });
+    assert.equal(success, true);
+
+    // Checks logs
+    let receipt = await tokenInstance.transferFrom(from, to, 10, { from: spender });
+    assert.equal(receipt.logs.length, 1, 'Triggers one event');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the "Transfer" event');
+    assert.equal(receipt.logs[0].args._from, from, 'Logs the account the tokens are transferred from');
+    assert.equal(receipt.logs[0].args._to, to, 'Logs the account the tokens are transferred to');
+    assert.equal(receipt.logs[0].args._value, 10, 'Logs the amount transferred');
+
+    // Sends and receives proper balance
+    let balanceFrom = await tokenInstance.balanceOf(from);
+    assert.equal(balanceFrom.toNumber(), 90, 'Deducts the correct amount from sending account [from]');
+    let balanceTo = await tokenInstance.balanceOf(to);
+    assert.equal(balanceTo.toNumber(), 10, 'Adds the correct amount to the receiving account [to]');
+
+    // Updates allowance
+    let allowance = await tokenInstance.allowance(from, spender);
+    assert.equal(allowance, 0, 'Deducts amount from allowance');
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
